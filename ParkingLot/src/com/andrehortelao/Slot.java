e4fbc50df317054fa4535fae9d293c420f3a0f5a
André Hortelão <andrefhortelao@gmail.com>
2024-04-26 15:23:17 +0100

    Simple implementation

---
+++ ParkingLot/src/com/andrehortelao/Slot.java
@@ -0,0 +1,37 @@
+package com.andrehortelao;
+
+public class Slot {
+    private String type;
+    private Vehicle vehicle;
+    private String ticketId;
+
+    public Slot(String type) {
+        this.type = type;
+        this.vehicle=null;
+        this.ticketId=null;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+}
---
+++ ParkingLot/src/com/andrehortelao/Vehicle.java
@@ -0,0 +1,13 @@
+package com.andrehortelao;
+
+public class Vehicle {
+    private String type;
+    private String registration;
+    private String color;
+
+    public Vehicle(String type, String registration, String color) {
+        this.type = type;
+        this.registration = registration;
+        this.color = color;
+    }
+}
